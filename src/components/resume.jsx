03eb7ef9d332fa1e19fc03d83dac8ec26bcb7764
Aditya Khowal <adityakh2003@outlook.com>
2023-05-07 03:29:25 -0700

    first commit

---
+++ src/components/resume.jsx
@@ -0,0 +1,29 @@
+
+import React, { useEffect } from 'react'
+import pdfURL from '../img/ResumeMarch5.pdf';
+import Navbar from './navbar';
+const PDFViewer = () => {
+    useEffect(() => {
+        document.body.classList.add('resume-page');
+
+        return () => {
+            document.body.classList.remove('resume-page');
+        };
+    }, []);
+    return (
+
+        // import navbar before iframe
+        <>
+            <Navbar />
+            <div>
+                <iframe
+                    src={`${pdfURL}#toolbar=0`}
+                    class="pdf"
+                />
+            </div>
+        </>
+    );
+};
+
+
+export default PDFViewer;
